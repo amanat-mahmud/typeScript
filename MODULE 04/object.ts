ff23aecf6970051666688e8ba67c68b63036653c
AmanatUMahmud <amanatmahmud@gmail.com>
2023-05-04 22:25:13 +0600

    inheritance added

--- MODULE 04/object.ts
+++ MODULE 04/object.ts
@@ -1,8 +1,8 @@
-class  Student{
+class  Student1{
     constructor(public name:string,public id:string,public semester:number){}
     public printInfo(){
         console.log(`${this.name}. ID - ${this.id}. Semester - ${this.semester}`)
     }
 }
-const amanat = new Student('Amanat',"C201055",6);
-amanat.printInfo()
+const amanat55 = new Student1('Amanat',"C201055",6);
+amanat55.printInfo();
